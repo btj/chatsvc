7e401a3a1a1a8e186b2e8f2c6fffc398de772c32
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-08-01 08:16:20 +0200

    Add NameConstraints to root cert

--- tls-key-generation/create-keys.sh
+++ tls-key-generation/create-keys.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-openssl genrsa -des3 -out ../../rootCA.key 2048
-openssl req -x509 -new -subj '/CN=chatsvcroot' -nodes -key ../../rootCA.key -sha256 -days 1024 -out ../../rootCA.pem
+set -e -x
+openssl genrsa -aes256 -out ../../rootCA.key 2048
+openssl req -new -key ../../rootCA.key -out ../../rootCA.csr -subj '/CN=chatsvc.mooo.com trust CA'
+openssl x509 -req -sha256 -days 1024 -in ../../rootCA.csr -signkey ../../rootCA.key -extfile rootCAext.ini -out ../../rootCA.pem
 
 openssl req -new -sha256 -nodes -out ../../server.csr -newkey rsa:2048 -keyout ../../server.key -config server.csr.cnf
 openssl x509 -req -in ../../server.csr -CA ../../rootCA.pem -CAkey ../../rootCA.key -CAcreateserial -out ../../server.crt -days 500 -sha256 -extfile v3.ext
+cat ../../server.crt ../../rootCA.pem > ../../server_and_ca.crt
---
+++ tls-key-generation/key_to_env.sh
@@ -0,0 +1,2 @@
+export CHATSVC_TLS_CERT=$(base64 -i ../../server_and_ca.crt)
+export CHATSVC_TLS_PRIVATE_KEY=$(base64 -i ../../server.key)
